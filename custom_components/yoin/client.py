f3ce1c596142b7510edc7582536a37bfa1544163
Geert Meersman <geertmeersman@gmail.com>
2024-04-17 17:42:28 +0200

    fix: unused imports removed

--- custom_components/yoin/client.py
+++ custom_components/yoin/client.py
@@ -53,8 +53,8 @@ class YoinClient:
         headers = self._headers
         headers.update(
             {
-                "Referer": f"https://my.yoin.be/login",
-                "Origin": f"https://my.yoin.be",
+                "Referer": "https://my.yoin.be/login",
+                "Origin": "https://my.yoin.be",
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
             }
         )
